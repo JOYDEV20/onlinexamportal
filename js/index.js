a7118c82ad626d19ee0e5c9ca798fc9c2dd1ef5e
JOYDEV20 <50595733+JOYDEV20@users.noreply.github.com>
2026-07-20 12:03:09 +0530

    Resolve merge conflicts and update exam duration/question count

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 document
    .getElementById(
       "startBtn"
@@ -11,18 +10,4 @@ document
             "login.html";
 
       }
-=======
-document
-   .getElementById(
-      "startBtn"
-   )
-   .addEventListener(
-      "click",
-      function () {
-
-         window.location =
-            "login.html";
-
-      }
->>>>>>> ab7ecf0c5f5f48419b5469d4f7a220f7c394d8fa
    );
--- js/protect.js
+++ js/protect.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const student =
 JSON.parse(
   localStorage.getItem(
@@ -14,21 +13,4 @@ if (!student) {
 document
   .getElementById("name")
   .innerHTML =
-=======
-const student =
-JSON.parse(
-  localStorage.getItem(
-    "student"
-  )
-);
-
-if (!student) {
-  window.location =
-    "login.html";
-}
-
-document
-  .getElementById("name")
-  .innerHTML =
->>>>>>> ab7ecf0c5f5f48419b5469d4f7a220f7c394d8fa
   student.name;
